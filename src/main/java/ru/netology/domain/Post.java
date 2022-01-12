53aad9834874f5132ba7ca5ca1a727c5b24813bd
Konstantin <koc.voytenko@gmail.com>
2022-01-12 09:45:37 +0300

    Vk v1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private String postLogoURL;
+    private String profileName;
+    private boolean hidePost;
+    private boolean favorite;
+    private int date;
+    private String mainText;
+    private String imageURL;
+    private int likesCount;
+    private boolean commentButton;
+    private boolean repostButton;
+    private int repostCount;
+    private int viewsCount;
+}
